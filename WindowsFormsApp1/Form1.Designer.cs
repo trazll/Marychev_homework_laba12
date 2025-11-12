0f87332db01c1deae38867c889d38446b906df9b
traz <degleted@gmail.com>
2025-11-12 22:15:08 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblClicks = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick.Location = new System.Drawing.Point(26, 251);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(332, 125);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "ЖМИ СЮДА!";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(414, 273);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(137, 73);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblClicks
+            // 
+            this.lblClicks.AutoSize = true;
+            this.lblClicks.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblClicks.Location = new System.Drawing.Point(17, 149);
+            this.lblClicks.Name = "lblClicks";
+            this.lblClicks.Size = new System.Drawing.Size(234, 54);
+            this.lblClicks.TabIndex = 2;
+            this.lblClicks.Text = "Нажатий: 0";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTimer.Location = new System.Drawing.Point(17, 68);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(261, 54);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.Text = "Время: 00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblClicks);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblClicks;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int clickCount = 0;
+        private int seconds = 0;
+        private bool isTimerRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            lblClicks.Text = $"Кликов: {clickCount}";
+
+            // Запускаем таймер при первом клике
+            if (!isTimerRunning)
+            {
+                timer1.Start();
+                isTimerRunning = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            int minutes = seconds / 60;
+            int secs = seconds % 60;
+            lblTimer.Text = $"Время: {minutes:00}:{secs:00}";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            clickCount = 0;
+            seconds = 0;
+            isTimerRunning = false;
+
+            lblClicks.Text = "Кликов: 0";
+            lblTimer.Text = "Время: 00:00";
+
+            timer1.Stop();
+        }
+
+    }
+}
